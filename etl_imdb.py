bfea00460ff999ec47bb91b40e295891245cbf33
Emersonmiady <emersonmiady@gmail.com>
2025-03-30 22:51:54 -0300

    Added logging and modularization to the code

--- etl_imdb.py
+++ etl_imdb.py
@@ -1,207 +1,268 @@
 ### IMPORTS
 import os
+import sqlite3
+import logging
 import requests
 import pandas as pd
-import sqlite3
 
-### EXTRACT
+# Logging config
+log_format = '%(asctime)s - %(levelname)s - %(message)s'
+logging.basicConfig(level=logging.DEBUG, format=log_format)
 
-# Download files
+#log_format: Defines the format of log messages, including:
+#
+#   - %(asctime)s: Timestamp (when the message was logged).
+#
+#   - %(levelname)s: Log level (DEBUG, INFO, WARNING, etc.).
+#
+#   - %(message)s: The actual message.
+#
+#logging.basicConfig(): Sets the log detail level to DEBUG and applies the defined format.
 
-# URL
-db_url = "https://datasets.imdbws.com/"
 
-# File names from URL
-files = [
-    "name.basics.tsv.gz",
-    "title.akas.tsv.gz",
-    "title.basics.tsv.gz",
-    "title.crew.tsv.gz",
-    "title.episode.tsv.gz",
-    "title.principals.tsv.gz",
-    "title.ratings.tsv.gz"
-]
+# Add a handler for files
+log_file_path = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'script_logs.log')
+file_handler = logging.FileHandler(log_file_path)
+file_handler.setLevel(logging.DEBUG)
+file_handler.setFormatter(logging.Formatter(log_format))
+logging.getLogger().addHandler(file_handler)
+
+#log_file_path: Sets the absolute path of the file where the logs will be saved (script_logs.log) in the same directory as the script.
+#
+#FileHandler: Creates a file handler that logs the messages to the specified file.
+#
+#setLevel(logging.DEBUG): Sets the minimum level of messages that will be logged to the file.
+#
+#setFormatter: Applies the same log format to the file, using log_format.
+#
+#addHandler: Adds the file handler to the main logger so that the logs are written to both the console and the file.
+
 
 # Local folder
-data_path = "data"
+DATA_PATH = 'data'
+PREPROCESSED_DATA_PATH = os.path.join(DATA_PATH, 'preprocessed')
 
-# Check: path exists
-os.makedirs(data_path, exist_ok=True)
+# URL
+DB_URL = 'https://datasets.imdbws.com/'
 
-# Download files
-for file in files:
-    url = db_url + file
-    local_path = os.path.join(data_path, file)
+# DB
+DB = 'imdb_data.db'
 
-    # If the file exists, then ignore, else download locally
-    if not os.path.exists(local_path):
-        print(f'Downloading {file}...')
-        response = requests.get(url)
+# File names from URL
+GZ_FILES = [
+    'name.basics.tsv.gz',
+    'title.akas.tsv.gz',
+    'title.basics.tsv.gz',
+    'title.crew.tsv.gz',
+    'title.episode.tsv.gz',
+    'title.principals.tsv.gz',
+    'title.ratings.tsv.gz'
+]
 
-        # Request done? (status = 200)
-        if response.status_code == 200:
-            with open(local_path, 'wb') as f:
-                f.write(response.content)
-            print(f'{file} downloaded!')
+
+
+
+def extract(
+        db_url: str = DB_URL,
+        data_path: str = DATA_PATH,
+        files: list = GZ_FILES
+) -> None:
+    """
+    TODO: Docstring.
+    """
+
+    os.makedirs(data_path, exist_ok=True)
+
+    for file in files:
+        url = db_url + file
+        local_path = os.path.join(data_path, file)
+
+        if not os.path.exists(local_path):
+            logging.info(f'Downloading {file}...')
+            response = requests.get(url)
+
+            if response.status_code == 200:
+                with open(local_path, 'wb') as f:
+                    f.write(response.content)
+                logging.info(f'{file} downloaded!')
+            else:
+                logging.error(f'Download failed... Status code: {response.status_code}')
+            
+            del response
+
         else:
-            print(f'Download failed... Status code: {response.status_code}')
-    else:
-        print(f'{file} already exists.')
+            logging.info(f'{file} already exists. The process will continue.')
 
-print('Download concluded!')
+    logging.info('All downloads were concluded!')
 
-### TRANSFORM
 
-# Directories
-data_path = "data"
-data_preprocessed_path = os.path.join(data_path, "preprocessed")
 
-# Check: path exists
-os.makedirs(data_preprocessed_path, exist_ok=True)
 
-# Files
-files = os.listdir(data_path)
+def transform(
+        data_path: str = DATA_PATH,
+        preprocessed_data_path: str = PREPROCESSED_DATA_PATH,
+        files: list = GZ_FILES
+) -> None:
+    """
+    TODO: Docstring.
+    """
 
-# Open, preprocess and save data
-for file in files:
-    file_path = os.path.join(data_path, file)
+    os.makedirs(preprocessed_data_path, exist_ok=True)
 
-    if os.path.isfile(file_path) and file.endswith('.gz'):
-        print(f'Reading and preprocessing the file {file}...')
+    for file in files:
+        file_path = os.path.join(data_path, file)
 
-        # Read the file and transform to pandas
-        df = pd.read_csv(file_path, sep='\t', compression='gzip', low_memory=False)
+        if os.path.isfile(file_path) and file.endswith('.gz'):
+            logging.debug(f'Reading and preprocessing the file {file}...')
 
-        # Replace "\\N" by NaN
-        df.replace({'\\N': None}, inplace=True)
+            df = pd.read_csv(
+                file_path,
+                sep='\t',
+                compression='gzip',
+                low_memory=False,
+                nrows=1_000
+            )
 
-        # Save the DataFrame on preprocessed folder without compression
-        destiny_path = os.path.join(data_preprocessed_path, file[:-3]) # remove .gz extension
-        df.to_csv(destiny_path, sep='\t', index=False)
+            df.replace({'\\N': None}, inplace=True)
 
-        print(f'Preprocess concluded for {file}! File saved on {data_preprocessed_path}')
+            destiny_path = os.path.join(preprocessed_data_path, file[:-3]) # remove .gz extension
+            df.to_csv(destiny_path, sep='\t', index=False)
 
-print(f'All the files were preprocessed and downloaded on "preprocessed" directory!')
+            logging.debug(f'Preprocess concluded for {file}! File saved on {preprocessed_data_path}')
 
-### LOAD RAW TABLES
+            os.remove(file_path)
 
-# Save on DB using SQLite
+    logging.info(f'All the files were preprocessed and downloaded on "preprocessed" directory!')
 
-# Directories
-preprocessed_path = os.path.join("data", "preprocessed")
-db = "imdb_data.db"
 
-# Connect to SQLite DB
-conn = sqlite3.connect(db)
 
-# List all the saved files
-files = os.listdir(preprocessed_path)
 
-# Read and save the files into DB
-for file in files:
-    file_path = os.path.join(preprocessed_path, file)
+def load(
+        conn,
+        file_path: str = DATA_PATH,
+        preprocessed_data_path: str = PREPROCESSED_DATA_PATH
+) -> None:
+    """
+    TODO: Docstring.
+    """
 
-    if os.path.isfile(file_path) and file.endswith('.tsv'):
-        # Read the TSV file using pandas
-        df = pd.read_csv(file_path, sep='\t', low_memory=False)
+    files = os.listdir(preprocessed_data_path)
 
-        # Remove extension from the file name
-        table_name = os.path.splitext(file)[0]
+    for file in files:
+        file_path = os.path.join(preprocessed_data_path, file)
 
-        # Replace special characters in the file name
-        table_name = table_name.replace('.', '_').replace('-', '_')
+        if os.path.isfile(file_path) and file.endswith('.tsv'):
 
-        # Save the DataFrame into DB
-        df.to_sql(table_name, conn, index=False, if_exists='replace')
+            df = pd.read_csv(file_path, sep='\t', low_memory=False)
 
-        print(f'File {file} was saved as table {table_name} and uploaded into the SQLite DB.')
+            table_name = os.path.splitext(file)[0]
 
-# Close the connection with DB
-conn.close()
+            table_name = table_name.replace('.', '_').replace('-', '_')
 
-print('All the files were saved into DB!')
+            df.to_sql(table_name, conn, index=False, if_exists='replace')
 
-### LOAD ANALYTICAL TABLES
+            logging.info(f'File {file} was saved as table {table_name} and uploaded into the SQLite DB.')
 
-analytics_titles = """
-CREATE TABLE IF NOT EXISTS analytics_titles AS
+    logging.info('All the files were saved into DB!')
 
-WITH principals AS (
-    SELECT
-        tconst,
-        COUNT(DISTINCT nconst) AS numParticipants
-    FROM title_principals
-    GROUP BY 1
-),
 
-languages AS (
-    SELECT DISTINCT
-        titleId AS tconst,
-        language
-    FROM title_akas
-    WHERE language IS NOT NULL
-)
 
-SELECT DISTINCT
-    tb.tconst,
-    tb.titleType,
-    tb.originalTitle,
-    tb.startYear,
-    tb.endYear,
-    tb.genres,
-    ta.language,
-    tr.averageRating,
-    tr.numVotes,
-    tp.numParticipants
-FROM title_basics AS tb
-LEFT JOIN languages AS ta
-    ON tb.tconst = ta.tconst
-LEFT JOIN title_ratings AS tr
-    ON tb.tconst = tr.tconst
-LEFT JOIN principals AS tp
-    ON tb.tconst = tp.tconst
-"""
 
-analytics_principals = """
-CREATE TABLE IF NOT EXISTS analytics_principals AS
+def create_views(
+        conn
+) -> None:
+    """
+    TODO: Docstring.
+    """
 
-SELECT DISTINCT
-    tp.nconst,
-    tp.tconst,
-    tp.ordering,
-    tp.category,
-    tb.genres
-FROM title_principals AS tp
-LEFT JOIN title_basics AS tb
-    ON tp.tconst = tb.tconst
-"""
+    analytics_titles = '''
+    CREATE TABLE IF NOT EXISTS analytics_titles AS
 
-# Lists of create table queries
-create_table_queries = [analytics_titles, analytics_principals]
+    WITH principals AS (
+        SELECT
+            tconst,
+            COUNT(DISTINCT nconst) AS numParticipants
+        FROM title_principals
+        GROUP BY 1
+    ),
 
-# DB name
-db = 'imdb_data.db'
-
-# Save tables into DB
-create_table_queries_indexes = zip(
-    create_table_queries,
-    range(1, len(create_table_queries)) # Index for print
+    languages AS (
+        SELECT DISTINCT
+            titleId AS tconst,
+            language
+        FROM title_akas
+        WHERE language IS NOT NULL
     )
 
-for query, i in create_table_queries_indexes:
+    SELECT DISTINCT
+        tb.tconst,
+        tb.titleType,
+        tb.originalTitle,
+        tb.startYear,
+        tb.endYear,
+        tb.genres,
+        ta.language,
+        tr.averageRating,
+        tr.numVotes,
+        tp.numParticipants
+    FROM title_basics AS tb
+    LEFT JOIN languages AS ta
+        ON tb.tconst = ta.tconst
+    LEFT JOIN title_ratings AS tr
+        ON tb.tconst = tr.tconst
+    LEFT JOIN principals AS tp
+        ON tb.tconst = tp.tconst
+    '''
 
-    # Connection to DB
-    conn = sqlite3.connect(db)
+    analytics_principals = '''
+    CREATE TABLE IF NOT EXISTS analytics_principals AS
 
-    # Cursor
+    SELECT DISTINCT
+        tp.nconst,
+        tp.tconst,
+        tp.ordering,
+        tp.category,
+        tb.genres
+    FROM title_principals AS tp
+    LEFT JOIN title_basics AS tb
+        ON tp.tconst = tb.tconst
+    '''
+
+    queries = [
+        'DROP TABLE analytics_titles', # We have to drop existing tables
+        'DROP TABLE analytics_principals',     # to update with new data
+        analytics_titles,
+        analytics_principals
+        ]
+
     cursor = conn.cursor()
 
-    # Execute query
-    cursor.execute(query)
+    for query in queries:
 
-    # Close connection
+        cursor.execute(query)
+
+    logging.info('Analytical tables were created succesfully!')
+    logging.info('ETL finished!')
+
+
+
+
+if __name__ == '__main__':
+
+    extract()
+    transform()
+
+    conn = sqlite3.connect(DB)
+
+    load(conn=conn)
+    create_views(conn=conn)
+
     conn.close()
 
-    print(f'Analytical table {i} was created succesfully.')
-
-print('Tables were created succesfully!')
+## Schedule the script execution
+#schedule.every().day.at('09:00').do(execute_imdb_etl_script)
+#
+## Execute the script
+#while True:
+#    schedule.run_pending() # Verify pending tasks on schedule object
+#    time.sleep(1) # Wait 1s
